2a1a281f3500a90fe9434a326074eb7deb905347
Mahib Rahman <mahib.7009@gmail.com>
2025-02-09 17:55:14 -0500

    adding hot reload fail

--- Mahib Test/Backend/server.js
+++ Mahib Test/Backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("server is ready now");
+});
+
 app.listen(5000, () => {
-    console.log("Server started at 5000");
+    console.log("Server started at 5000 fs");
 });
